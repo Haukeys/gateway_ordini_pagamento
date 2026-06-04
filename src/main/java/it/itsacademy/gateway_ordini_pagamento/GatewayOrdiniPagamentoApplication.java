10c4d888a521384e315889a4343f9d09a11f4b12
Haukeys <leopazienza76@gmail.com>
2026-06-04 17:32:00 +0200

    new gateway

---
+++ src/main/java/it/itsacademy/gateway_ordini_pagamento/GatewayOrdiniPagamentoApplication.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gateway_ordini_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayOrdiniPagamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayOrdiniPagamentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/itsacademy/gateway_ordini_pagamento/GatewayOrdiniPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gateway_ordini_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayOrdiniPagamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
